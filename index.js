e4a25337cb441535e2e50279c5dda80eb67728db
Gerrit van Huyssteen <gerrit@brazenhill.com>
2016-06-13 13:20:32 +0200

    Refact: Cleaned up and made it simple to use

--- index.js
+++ index.js
@@ -1,51 +1,121 @@
+'use strict';
+
 var fs = require('fs');
+var open = require('open');
 var speedTest = require('speedtest-net');
-var schedule = require('node-schedule');
+var nodeSchedule = require('node-schedule');
+var args = require('minimist')(process.argv.slice(2));
 
-var j = schedule.scheduleJob('*/2 * * * *', function(){
+const INTERVAL_ONCE = 'once';
+const defaults = {
+  'interval': 5
+};
 
-  test = speedTest({maxTime: 5000});
+var interval = defaults.interval;
 
-  test.on('data', function(data) {
+/**
+ * Returns the help instructions
+ */
+if ((args.h)||(args.help)) {
+  console.log("\n");
+  console.log("Internet speed test scheduler");
+  console.log("Supply a more detailed report to your DSL provider");
+  console.log("————————————————————————————————————————————————————————————————————————————————");
+  console.log("node .                        Runs a default speed test of 5 minute intervals");
+  console.log("node . -i "+INTERVAL_ONCE+"                Runs a once off speed test");
+  console.log("node . -i 5                   Sets and interval of 5 minutes");
+  console.log("\n");
+  process.exit(0);
+}
+
+/**
+ * Checks if -i flag value is valid
+ */
+if(args.i) {
+  var validIntervals = [INTERVAL_ONCE, 2, 5, 10, 20, 30];
+
+  if(validIntervals.indexOf(args.i) === -1){
+    console.error('Valid intervals: ' + validIntervals.toString());
+    process.exit(0);
+  }
+
+  interval = args.i;
+}
+
+/**
+ * Starts scheduled speed test
+ */
+var runScheduler = function(interval) {
+  open('index.html');
+  console.log('Scheduler starting. First test will run in ' + interval + 'minutes.');
+  nodeSchedule.scheduleJob('*/'+interval+' * * * *', function () {
+    runSpeedTest();
+  });
+};
+
+/**
+ * Starts once off speed test
+ */
+var runOnceOff = function() {
+  console.log('Once off speed test starting...');
+  runSpeedTest();
+};
+
+/**
+ * Runs the speed test
+ * Timeout: 5000seconds
+ */
+var runSpeedTest = function() {
+  var test = speedTest({maxTime: 5000});
+
+  test.on('data', function (data) {
     writeData(data);
   });
 
-  test.on('error', function(err) {
+  test.on('error', function (err) {
     console.error(err);
   });
-
-});
-
-var writeData = function(data){
-  var fileContent = 'Speedtest done at: ' + getDateTime() + "\n\n";
-  fileContent += JSON.stringify(data, null, 4);
-
-  fs.writeFile("./tests/" + getDateTime(true) + ".txt", fileContent, function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    console.log("The data file was saved!");
-  });
-
-  var log = {
-    'date': getDateTime(),
-    'download': data.speeds.download,
-    'upload': data.speeds.upload,
-    'ping': data.server.ping
-  };
-  fs.writeFile("./log/" + getDateTime(true) + ".txt", JSON.stringify(log), function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    console.log("The log file was saved!");
-  });
-
-  console.log(JSON.stringify(log));
-
 };
 
-function getDateTime(isFileName) {
+/**
+ * Writes and returns the speed test results
+ * @param data
+ */
+var writeData = function(data){
 
+  data.date = getDateTime();
+
+  fs.mkdir("./logs", function(err){ });
+
+  fs.writeFile("./logs/" + getDateTime(true) + ".txt", JSON.stringify(data, null, 4), function(err) {
+    if (err) throw err;
+  });
+
+  fs.readFile("data.js", function (err, fileData) {
+    if (err) {
+      fileData = "var data = [];\n";
+    }
+
+    fs.writeFile("data.js", fileData + "data.push(" + JSON.stringify(data) + ");\n", function (err) {
+      if (err) throw err;
+    });
+  });
+
+  console.log("\n");
+  console.log('Date: ' + getDateTime());
+  console.log('Ping: ' + data.server.ping);
+  console.log('Download Speed: ' + data.speeds.download);
+  console.log('Upload Speed: ' + data.speeds.upload);
+  console.log("\n");
+};
+
+/**
+ * Returns the date and time in a proper format
+ *
+ * @param isFileName
+ * @returns {*}
+ */
+function getDateTime(isFileName) {
   var date = new Date();
 
   var hour = date.getHours();
@@ -70,5 +140,9 @@ function getDateTime(isFileName) {
   }else{
     return year + "-" + month + "-" + day + " " + hour + ":" + min + ":" + sec;
   }
-
 }
+
+/**
+ * Runs the script
+ */
+if(interval === INTERVAL_ONCE) runOnceOff(); else runScheduler(interval);
